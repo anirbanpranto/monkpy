845cac8517ff296a7a80a71bf94e3b6e0ccbbcd7
anirbanpranto <anirbanpranto@gmail.com>
2023-06-18 02:35:55 +0800

    feat : initial lexer

---
+++ tokens.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from enum import Enum
+
+class TokenType(Enum):
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+
+    IDENT = "IDENTIFIER"
+    INT = "INTEGER"
+
+    ASSIGN = "="
+    PLUS = "+"
+
+    COMMA = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACES = "{"
+    RBRACES = "}"
+
+    FUNCTION = "FUNCTION"
+    LET = "LET"
+
+@dataclass
+class Token:
+    type : TokenType
+    literal : str
